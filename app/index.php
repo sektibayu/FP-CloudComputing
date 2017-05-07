9dc85b4373a12e1504545bc16f709d8ad5ce51af
bayu <sektibayu@gmail.com>
2017-05-07 21:22:17 +0700

    index app

---
+++ app/index.php
@@ -0,0 +1,11 @@
+ <form action="auth/login.php">
+  <div class="container">
+    <label><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="Username" required>
+
+    <label><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="password" required>
+
+    <button type="submit">Login</button>
+  </div>
+</form>
